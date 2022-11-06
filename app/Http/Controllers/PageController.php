eb1448db3b3564f7a1daec41dcfade6a5650e394
Carmyo <carmelodicristina@gmail.com>
2022-11-06 18:28:30 +0100

    done

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Movie;
+class PageController extends Controller
+{
+    //
+    public function index(){
+        $movies = Movie::all();
+        dd($movies);        
+    }
+}
+
